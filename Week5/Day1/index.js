240c4b783ac8133d47b168803a553ca7e018f2dc
Shobhit Raj <shobhitr.hestabit@gmail.com>
2025-12-07 13:34:31 +0530

    Day1

--- Week5/Day1/index.js
+++ Week5/Day1/index.js
@@ -1,1 +1,11 @@
-console.log("Node app running inside Docker container...");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Node app running inside Docker container!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
